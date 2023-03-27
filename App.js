aaa70ba831feab0f306f478d8bb02783a98caa73
khubaib-ali-codegenio <117726252+khubaib-ali-codegenio@users.noreply.github.com>
2023-03-27 13:16:13 +0500

    Draw Circle and Oval

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {Canvas, Circle, Oval} from "@shopify/react-native-skia";
+import {useWindowDimensions} from 'react-native';
 
 export default function App() {
+    const {height, width, scale, fontScale} = useWindowDimensions();
+    const center = {x: width / 2, y: height / 2}
+    const rct = {x: center.x - 90, y: center.y - 50, width: 180, height: 100}
     return (
-        <View style={styles.container}>
-            <Text>Hello, I want to learn skia</Text>
-            <StatusBar style="auto"/>
-        </View>
-    );
+        <Canvas style={{flex: 1}}>
+            <Circle r={25} color={"lightblue"} c={center}/>
+            <Oval rect={rct} color="lightblue" style="stroke" strokeWidth={18}/>
+        </Canvas>
+    )
 }
 
 const styles = StyleSheet.create({
