ff986ad1c473d8cebd9fa304d7eb795cad025d5e
khubaib-ali-codegenio <117726252+khubaib-ali-codegenio@users.noreply.github.com>
2023-03-27 14:40:26 +0500

    Add Sweep and Radial Gradient

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import {Canvas, Circle, Group, Oval} from "@shopify/react-native-skia";
+import {Canvas, Circle, Group, Oval, Paint, RadialGradient, Rect, SweepGradient, vec} from "@shopify/react-native-skia";
 import {useWindowDimensions} from 'react-native';
 
 export default function App() {
     const {height, width, scale, fontScale} = useWindowDimensions();
-    const center = {x: width / 2, y: height / 2}
+    const center = vec(width / 2, height / 2)
     const rct = {x: center.x - 90, y: center.y - 50, width: 180, height: 100}
     return (
         <Canvas style={{flex: 1}}>
-            <Circle r={25} color={"lightblue"} c={center}/>
-            <Group color={"lightblue"} style={"stroke"} strokeWidth={18}>
+            <Circle r={25} c={center}>
+                <RadialGradient c={vec(center.x + 25, center.y - 10)} r={50} colors={["lightblue", "cyan"]}/>
+            </Circle>
+            {/*<Paint style={"stroke"} strokeWidth={18}>*/}
+            {/*    <SweepGradient c={vec(128, 128)} colors={["blue", 'red', 'blue']}/>*/}
+            {/*</Paint>*/}
+            <Group color={"lightblue"} style={"stroke"} strokeWidth={10}>
                 <Oval rect={rct}/>
+                <Group transform={[{rotate: Math.PI / 3}, {scale: -1}]} origin={center}>
+                    <Oval rect={rct}/>
+                </Group>
+                <Group transform={[{rotate: -Math.PI / 3}, {scale: -1}]} origin={center}>
+                    <Oval rect={rct}/>
+                </Group>
+                <SweepGradient
+                    c={vec(100, 100)}
+                    colors={["cyan", "magenta", "yellow",]}
+                />
             </Group>
         </Canvas>
     )
